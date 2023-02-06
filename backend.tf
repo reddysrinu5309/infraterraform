fdbf041a59738a1ddafdb14524ec81369610c36b
reddysrinu5309 <117962843+reddysrinu5309@users.noreply.github.com>
2023-02-06 11:47:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "hashtek-terraform-state-bucket"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "hashtekTerraformStatelock"
+    dynamodb_table = "terraformjenkinslearnin"
   }
 }
